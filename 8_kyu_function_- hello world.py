dd8bb9e7a30a4ba0ae41e98ab76586b2c7482936
Spencer Meredith <Smeredith1119@gmail.com>
2019-09-13 09:53:01 -0700

    Added the first problem kyu 8 from codewars

---
+++ 8_kyu_function_- hello world.py
@@ -0,0 +1,18 @@
+This Ss The Problem
+Description:
+Make a simple function called greet that returns the most-famous "hello world!".
+
+Style Points
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+
+
+
+def greet():
+   return("hello world!")
+
+greet()
+
+
+# how come when i remove greet() it works but when i dont remove it works still
+# also before i did print("hello world!") that doesnt work on this because it
